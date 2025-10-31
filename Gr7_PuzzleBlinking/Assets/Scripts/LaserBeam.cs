278404fb80b98d151743b3918ebc8594ec73b634
Ollie2750 <oransb24@student.aau.dk>
2025-10-31 10:32:08 +0100

    Laser

--- Gr7_PuzzleBlinking/Assets/Scripts/LaserBeam.cs
+++ Gr7_PuzzleBlinking/Assets/Scripts/LaserBeam.cs
@@ -5,26 +5,20 @@ using UnityEngine.Rendering;
 public class LaserBeam : MonoBehaviour
 {
     private LineRenderer myLine;
-    [SerializeField] private Material beamMaterial;
-    
 
     private void Start()
     {
         myLine = GetComponent<LineRenderer>();
-        DrawLine(Vector3.zero, new Color(1f, 0f, 0f));
+        DrawLine(Vector3.zero);
     }
 
-    void DrawLine(Vector3 end, Color color)
+    void DrawLine(Vector3 end)
     {   
         Vector3 start = gameObject.transform.position;
-        myLine.material = beamMaterial; //new Material(Shader.Find("Transparent/Diffuse"));
-        myLine.startColor = color;
-        myLine.endColor = color;
-        myLine.startWidth = 0.1f;
-        myLine.endWidth = 0.1f;
+        myLine.startWidth = 0.2f;
+        myLine.endWidth = 0.2f;
         myLine.SetPosition(0, start);
         myLine.SetPosition(1, end);
-        //GameObject.Destroy(myLine, duration);
     }
 
 }
