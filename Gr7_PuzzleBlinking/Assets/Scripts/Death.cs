32b67b3bc4a960c915025390b803467a43bb0bc1
AReallyFatGoat <zachasaks@gmail.com>
2025-11-10 09:20:25 +0100

    Still trynna make deathbox

--- Gr7_PuzzleBlinking/Assets/Scripts/Death.cs
+++ Gr7_PuzzleBlinking/Assets/Scripts/Death.cs
@@ -5,21 +5,26 @@ public class Death : MonoBehaviour
 {
     public int maxHP = 100;
     public int playerHP;
+    public Vector3 spawnLocation = new Vector3(0, 0, 0);
 
     private void Start()
     {
-        playerHP = maxHP;
+        playerHP =+ maxHP;
     }
 
     private void OnTriggerEnter(Collider target)
     {
-        if (target.tag == "Player")
+        Debug.Log("Death triggered");
+        if (target.tag == "Death")
         {
             playerHP = playerHP - maxHP;
         }
     }
     void Update()
     {
-        
+        if (playerHP < 0)
+        {
+            transform.position = transform.position + spawnLocation;
+        }
     }
 }
