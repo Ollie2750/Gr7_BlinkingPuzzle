5c6a32d2e658aa6393ff6cffce5a5d0c255a5713
Nadi4019 <nhautopgross8@gmail.com>
2025-11-14 15:59:23 +0100

    UI promt

--- Gr7_PuzzleBlinking/Assets/Scripts/PillarRoom/PlayerInteractor.cs
+++ Gr7_PuzzleBlinking/Assets/Scripts/PillarRoom/PlayerInteractor.cs
@@ -8,6 +8,8 @@ public class PlayerInteractor : MonoBehaviour
     public float useDistance = 4f;        // how far you can “use” a button
     public bool showDebugRay = false;     // Draws a yellow ray for a moment when pressing E
 
+    public UIPromt uiPrompt;
+
     void Awake()
     {
         if (!cam) cam = Camera.main;
@@ -15,28 +17,55 @@ public class PlayerInteractor : MonoBehaviour
 
     void Update()
     {
-        // Optional visual aid in Scene/Game view
+        // Draw debug ray if enabled
         if (showDebugRay)
             Debug.DrawRay(cam.transform.position, cam.transform.forward * useDistance, Color.yellow);
 
-        if (Input.GetKeyDown(useKey))
-        {
-            // Ray from the camera forward
-            Ray ray = new Ray(cam.transform.position, cam.transform.forward);
-            if (Physics.Raycast(ray, out RaycastHit hit, useDistance))
-            {
-                // Button can be on the hit collider or a parent object
-                var pad = hit.collider.GetComponentInParent<ButtonPad>();
-                if (pad != null)
-                {
-                    pad.Activate();     // Delegate the actual spawn/respawn to the button
-                }
+        // Always raycast straight ahead from the camera
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+        bool canInteract = false;
 
-                var button = hit.collider.GetComponentInParent<TimedButton>();
-                if(button != null)
-                {
-                    button.activateBridge();
-                }
+        ButtonPad pad = null;
+        TimedButton button = null;
+
+        if (Physics.Raycast(ray, out RaycastHit hit, useDistance))
+        {
+            // Check if we are looking at a ButtonPad
+            pad = hit.collider.GetComponentInParent<ButtonPad>();
+
+            // Or a TimedButton
+            button = hit.collider.GetComponentInParent<TimedButton>();
+
+            if (pad != null || button != null)
+            {
+                canInteract = true;
+            }
+        }
+
+        // --- Handle UI prompt ---
+        if (uiPrompt != null)
+        {
+            if (canInteract)
+            {
+                uiPrompt.Show("Press E to interact");
+            }
+            else
+            {
+                uiPrompt.Hide();
+            }
+        }
+
+        // --- Handle actual interaction when E is pressed ---
+        if (canInteract && Input.GetKeyDown(useKey))
+        {
+            if (pad != null)
+            {
+                pad.Activate();
+            }
+
+            if (button != null)
+            {
+                button.activateBridge();
             }
         }
     }
