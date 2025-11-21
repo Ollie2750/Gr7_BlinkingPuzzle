b5c1eabdc8e1aa4c27f3a10c2a83dc70c2f5eafe
Ollie2750 <oransb24@student.aau.dk>
2025-11-21 10:27:21 +0100

    More piller

--- Gr7_PuzzleBlinking/Assets/Scripts/PillarRoom/SpawnPiller.cs
+++ Gr7_PuzzleBlinking/Assets/Scripts/PillarRoom/SpawnPiller.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using Unity.Netcode;
 
 public class SpawnPiller : Interactable
 {
@@ -7,6 +8,37 @@ public class SpawnPiller : Interactable
 
     public override void Interact()
     {
-        piller.transform.localPosition = spawnPosition;
+        // Request the server to move the pillar
+        if (NetworkManager.Singleton != null)
+        {
+            // Get the NetworkObject component from the pillar
+            NetworkObject pillerNetworkObject = piller.GetComponent<NetworkObject>();
+
+            if (pillerNetworkObject != null)
+            {
+                // Call the server RPC to move the pillar
+                RequestMovePillerServerRpc(pillerNetworkObject.NetworkObjectId);
+            }
+            else
+            {
+                Debug.LogError("Pillar does not have a NetworkObject component!");
+            }
+        }
+        else
+        {
+            // Fallback for single-player or testing
+            piller.transform.localPosition = spawnPosition;
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void RequestMovePillerServerRpc(ulong pillerNetworkId)
+    {
+        // Find the pillar's NetworkObject
+        if (NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(pillerNetworkId, out NetworkObject pillerNetObj))
+        {
+            // Move the pillar on the server
+            pillerNetObj.transform.localPosition = spawnPosition;
+        }
     }
 }
