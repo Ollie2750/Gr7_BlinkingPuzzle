5dabe06ac561ce578b067a5d28331a46e307f6ee
Ollie2750 <oransb24@student.aau.dk>
2025-11-21 11:23:47 +0100

    Back to basics

--- Gr7_PuzzleBlinking/Assets/Scripts/PillarRoom/SpawnPiller.cs
+++ Gr7_PuzzleBlinking/Assets/Scripts/PillarRoom/SpawnPiller.cs
@@ -1,48 +1,13 @@
 using Unity.Netcode;
 using UnityEngine;
 
-public class SpawnPiller : NetworkBehaviour
+public class SpawnPiller : Interactable
 {
     [SerializeField] private GameObject piller;
     [SerializeField] private Vector3 spawnPosition;
 
-    public void Interact()
+    public override void Interact()
     {
-        if (!IsServer)
-        {
-            // Client requests the server to move the pillar
-            RequestMovePillarServerRpc();
-        }
-        else
-        {
-            // Server moves the pillar directly
-            MovePillar();
-        }
-    }
-
-    [ServerRpc(RequireOwnership = false)]
-    private void RequestMovePillarServerRpc()
-    {
-        // Server executes the movement
-        MovePillar();
-    }
-
-    private void MovePillar()
-    {
-        // Move the pillar locally on the server
         piller.transform.localPosition = spawnPosition;
-
-        // Notify all clients to update their pillar position
-        UpdatePillarClientRpc(spawnPosition);
-    }
-
-    [ClientRpc]
-    private void UpdatePillarClientRpc(Vector3 newPosition)
-    {
-        if (!IsServer)
-        {
-            // Only clients update (server already moved it)
-            piller.transform.localPosition = newPosition;
-        }
     }
 }
