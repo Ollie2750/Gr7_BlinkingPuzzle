837a24754953a4b73e3aeade8aea9a3b3927c3bf
Ollie2750 <oransb24@student.aau.dk>
2025-11-21 11:46:16 +0100

    jaaa

--- Gr7_PuzzleBlinking/Assets/Scripts/Player/TimeFreezeAbility.cs
+++ Gr7_PuzzleBlinking/Assets/Scripts/Player/TimeFreezeAbility.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
 public class TimeFreezeAbility : MonoBehaviour
 {
@@ -24,6 +25,8 @@ public class TimeFreezeAbility : MonoBehaviour
 
     void Update()
     {
+        if (!IsOwner) return;
+
         if (Input.GetKeyDown(freezeKey) && !isOnCooldown && !isFreezeActive)
         {
             StartCoroutine(FreezeTime());
@@ -51,6 +54,8 @@ public class TimeFreezeAbility : MonoBehaviour
 
     void FreezeAllRigidbodies()
     {
+        if (!IsOwner) return;
+
         frozenRigidbodies.Clear();
 
         // Find all rigidbodies in the scene (using newer Unity API)
