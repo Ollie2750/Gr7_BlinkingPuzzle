ee06d873311356a915a7c19b26fb9e75a7ff6bb5
AReallyFatGoat <zachasaks@gmail.com>
2025-12-05 10:44:49 +0100

    Revert "Waiting for claude"
    This reverts commit b32bb78d9ff5b0b19ec08b787b8c399b72f46951.

--- Gr7_PuzzleBlinking/Assets/Scripts/PiP.cs
+++ Gr7_PuzzleBlinking/Assets/Scripts/PiP.cs
@@ -1,135 +1,24 @@
-﻿using System.Globalization;
 using UnityEngine;
-using UnityEngine.UI;
-using Unity.Netcode;
-using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
 
-public class PiP : NetworkBehaviour
+public class PiP : MonoBehaviour
 {
-    [SerializeField] private Camera playerCamera;
-    [SerializeField] private RawImage pipDisplayImage; // Drag the RawImage here
-    private ClientNetworkTransform clientNetworkTransform;
-
-    [SerializeField] private MonoBehaviour[] localOnlyScripts; // Drag your mouse look / controller scripts here
-
-    private RenderTexture myPipTexture;
-    private bool pipSetupComplete = false;
-
-
-    public override void OnNetworkSpawn()
+    private GameObject[] players;
+    [SerializeField] private RenderTexture pip;
+    void Update()
     {
-        base.OnNetworkSpawn();
-
-        if (playerCamera == null)
+        players = GameObject.FindGameObjectsWithTag("Player");
+        if (players.Length == 2)
         {
-            playerCamera = GetComponentInChildren<Camera>();
-        }
-
-        if (!IsOwner)
-        {
-            // This is a REMOTE player
-            if (playerCamera != null)
+            foreach (GameObject p in players)
             {
-                playerCamera.enabled = false;
+                if (p != gameObject)
+                {
+                    Camera cam = p.GetComponentInChildren<Camera>();
+                    cam.targetTexture = pip; 
+                }
             }
-
-            DisableLocalOnlyScripts();
-            return;
-        }
-
-        // This is the LOCAL player
-        SetupPiP();
-
-        // Start coroutine to keep trying until PiP is connected
-        StartCoroutine(KeepTryingToConnectPiP());
-    }
-
-    private void DisableLocalOnlyScripts()
-    {
-        foreach (var script in localOnlyScripts)
-        {
-            if (script != null)
-            {
-                script.enabled = false;
-            }
-        }
-
-        var characterController = GetComponent<CharacterController>();
-        if (characterController != null)
-        {
-            characterController.enabled = false;
-        }
-    }
-
-    private void SetupPiP()
-    {
-        myPipTexture = new RenderTexture(512, 512, 16);
-        myPipTexture.name = $"PiP_Texture_{OwnerClientId}";
-
-        if (pipDisplayImage != null)
-        {
-            pipDisplayImage.texture = myPipTexture;
-            Debug.Log($"Local player {OwnerClientId}: PiP texture created and assigned to UI");
-        }
-    }
-
-    private IEnumerator KeepTryingToConnectPiP()
-    {
-        // Keep trying every 0.5 seconds until we successfully connect
-        while (!pipSetupComplete)
-        {
-            yield return new WaitForSeconds(0.5f);
-            TryConnectToRemotePlayer();
-        }
-    }
-
-    private void TryConnectToRemotePlayer()
-    {
-        if (pipSetupComplete) return;
-
-        // Find ALL NetworkObjects in the scene
-        var allNetworkObjects = FindObjectsOfType<NetworkObject>();
-
-        Debug.Log($"Local player {OwnerClientId}: Searching for remote player. Found {allNetworkObjects.Length} network objects");
-
-        foreach (var netObj in allNetworkObjects)
-        {
-            // Skip if this is our own object
-            if (netObj.OwnerClientId == OwnerClientId) continue;
-
-            // Try to get PlayerPiP component
-            PiP remotePiP = netObj.GetComponent<PiP>();
-            if (remotePiP == null) continue;
-
-            // Found a remote player!
-            if (remotePiP.playerCamera != null && myPipTexture != null)
-            {
-                remotePiP.playerCamera.enabled = true;
-                remotePiP.playerCamera.targetTexture = myPipTexture;
-                remotePiP.playerCamera.depth = -10;
-
-                pipSetupComplete = true;
-
-                Debug.Log($"SUCCESS! Local player {OwnerClientId}: Connected to remote player {remotePiP.OwnerClientId}'s camera");
-                return;
-            }
-        }
-
-        Debug.Log($"Local player {OwnerClientId}: No remote player found yet, will keep trying...");
-    }
-
-    public override void OnNetworkDespawn()
-    {
-        base.OnNetworkDespawn();
-        CleanupRenderTexture();
-    }
-
-    private void CleanupRenderTexture()
-    {
-        if (myPipTexture != null)
-        {
-            myPipTexture.Release();
-            Destroy(myPipTexture);
         }
     }
 }
