3ca3099bac898e86a4127d4d547bf05633683f7d
AReallyFatGoat <zachasaks@gmail.com>
2025-12-05 11:33:21 +0100

    Fuck you unity

--- Gr7_PuzzleBlinking/Assets/Scripts/PiP.cs
+++ Gr7_PuzzleBlinking/Assets/Scripts/PiP.cs
@@ -1,24 +1,63 @@
 using UnityEngine;
 using System.Collections.Generic;
 using NUnit.Framework;
+using Unity.Netcode;
 
-public class PiP : MonoBehaviour
+public class PiP : NetworkBehaviour
 {
     private GameObject[] players;
-    [SerializeField] private RenderTexture pip;
-    void Update()
+    [SerializeField] private RenderTexture pipHost;
+    [SerializeField] private RenderTexture pipJoin;
+    [SerializeField] private MonoBehaviour[] localOnlyScripts;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Test")
+        {
+            GetPip();
+        }
+
+    }
+    public void GetPip()
     {
         players = GameObject.FindGameObjectsWithTag("Player");
         if (players.Length == 2)
         {
+            DisableLocalOnlyScripts();
             foreach (GameObject p in players)
             {
                 if (p != gameObject)
                 {
-                    Camera cam = p.GetComponentInChildren<Camera>();
-                    cam.targetTexture = pip; 
+                    if (IsOwner)
+                    {
+                        Camera cam = p.GetComponentInChildren<Camera>();
+                        cam.targetTexture = pipHost;
+                    }
+
+                    if (!IsOwner)
+                    {
+                        Camera cam = p.GetComponentInChildren<Camera>();
+                        cam.targetTexture = pipJoin;
+                    }
                 }
             }
+        }
+    }
+
+    private void DisableLocalOnlyScripts()
+    {
+        foreach (var script in localOnlyScripts)
+        {
+            if (script != null)
+            {
+                script.enabled = false;
+            }
+        }
+
+        var characterController = GetComponent<CharacterController>();
+        if (characterController != null)
+        {
+            characterController.enabled = false;
         }
     }
 }
